b7aec9865e8441dcee0efc890abafd60a2fddfd6
Dhruv kumar <36184412+Dhruv1573@users.noreply.github.com>
2023-06-27 08:40:59 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        struct Node* ans;
+        map<int,int> mp;
+        while(head1 || head2){
+            if(head1){
+                mp[head1->data]++;
+                head1=head1->next;
+            }
+            if(head2){
+                mp[head2->data]++;
+                head2=head2->next;
+            } 
+        }
+        ans=NULL,head1=NULL;
+        for(auto x : mp){
+            struct Node* newnode=new Node(x.first);
+            if(ans==NULL){
+                ans=newnode;head1=newnode;
+            }
+            else{
+                head1->next=newnode;
+                head1=head1->next;
+            }
+            mp.erase(x.first);
+        }
+        return ans;  
+    }
+};
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
